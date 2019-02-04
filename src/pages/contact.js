53c854a13520fdb8b849abcbb0d7638d5045eb5c
Quinn <quinnsmyers@gmail.com>
2019-02-03 21:16:19 -0800

    added first and last name labels

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,68 +1,74 @@
-import React, { Component } from 'react';
-import Layout from '../components/layout/layout';
-import Content from "../components/Content/Content"
-import "../components/styles/contact.sass"
-import Conform from '../components/conform/conform'
+import React, { Component } from "react";
+import Layout from "../components/layout/layout";
+import Content from "../components/Content/Content";
+import "../components/styles/contact.sass";
+import Conform from "../components/conform/conform";
 class contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        const data = this.props.data.contentfulSiteMisc
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const data = this.props.data.contentfulSiteMisc;
 
-        return (
-            <Layout>
-                <Content>
-                    <div className="contact">
-                        <div className="contact__title">
-                            <h2>Contact</h2>
-                        </div>
-                        <div className="contact__col col__swap">
-
-                            <div className="contact__col__left snap__full">
-                                <h3>get in touch</h3>
-                                <p>{data.contactEmail}</p>
-                            </div>
-                            <div className="contact__col__right snap__full">
-                                <Conform email={data.contactEmail}>
-                                    <label>
-                                        Name *
-                                        <div className="name">
-                                            <input required type="text" id="first" />
-                                            <input required type="text" id="last" />
-                                        </div>
-                                    </label>
-                                    <label>
-                                        Email Address *
-                                    <input required type="text" />
-                                    </label>
-                                    <label>
-                                        Subject *
-                                    <input required type="text" />
-                                    </label>
-                                    <label>
-                                        Message *
-                                    <textarea name="message" id="" cols="30" rows="10"></textarea>
-                                    </label>
-                                    <button type="submit" className="submit"> + </button>
-                                </Conform>
-                            </div>
-                        </div>
+    return (
+      <Layout>
+        <Content>
+          <div className="contact">
+            <div className="contact__title">
+              <h2>Contact</h2>
+            </div>
+            <div className="contact__col col__swap">
+              <div className="contact__col__left snap__full">
+                <h3>get in touch</h3>
+                <p>{data.contactEmail}</p>
+              </div>
+              <div className="contact__col__right snap__full">
+                <Conform email={data.contactEmail}>
+                  <label>
+                    Name *
+                    <div className="name">
+                      <div className="name__input">
+                        <input required type="text" id="first" />
+                        <p>First Name</p>
+                      </div>
+                      <div className="name__input">
+                        <input required type="text" id="last" />
+                        <p>Last Name</p>
+                      </div>
                     </div>
-                </Content>
-
-            </Layout>
-        );
-    }
+                  </label>
+                  <label>
+                    Email Address *
+                    <input required type="text" />
+                  </label>
+                  <label>
+                    Subject *
+                    <input required type="text" />
+                  </label>
+                  <label>
+                    Message *
+                    <textarea name="message" id="" cols="30" rows="10" />
+                  </label>
+                  <button type="submit" className="submit">
+                    {" "}
+                    +{" "}
+                  </button>
+                </Conform>
+              </div>
+            </div>
+          </div>
+        </Content>
+      </Layout>
+    );
+  }
 }
 export const query = graphql`
   query {
-    contentfulSiteMisc{
-        contactEmail
-        
-      }
+    contentfulSiteMisc {
+      contactEmail
+    }
   }
-`
+`;
 
 export default contact;
