8f00bfe8ce6229bfc2f6234a109069255ca02983
cole smith <colesmith@Coles-iMac.local>
2019-02-06 17:41:05 -0700

    bugfix

--- src/components/laxheader/laxheader.js
+++ src/components/laxheader/laxheader.js
@@ -14,6 +14,7 @@ class LaxHeader extends Component {
                     bgImage={this.props.bgimg}
                     bgImageAlt="the cat"
                     strength={300}
+                    bgImageStyle={{ marginTop: "4vw" }}
                 >
 
                     <Content>
@@ -22,7 +23,7 @@ class LaxHeader extends Component {
                 </Parallax>
 
 
-            </div>
+            </div >
 
         );
     }
